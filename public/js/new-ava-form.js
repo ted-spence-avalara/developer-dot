8ea4a2738aab8658546dda5ea9dc6b9c3d91bcfa
StephenAWalsh <steve.walsh@avalara.com>
2017-12-11 12:28:19 -0800

    added address fields to free trial form

--- public/js/new-ava-form.js
+++ public/js/new-ava-form.js
@@ -1,19 +1,30 @@
-function insertAvaForm()
-{
-    $(".avaform-wrapper").append("<div style='margin: 20px 20px 20px 20px; padding: 20px 20px 20px 20px;'><label>Request a Free AvaTax Sandbox Account</label><form><div class='form-group'><label for='name'>First Name / Given Name</label><input type='text' class='form-control' id='firstName' placeholder=''></div><div class='form-group'><label for='name'>Last Name / Family Name</label><input type='text' class='form-control' id='lastName' placeholder=''></div><div class='form-group'><label for='email'>Email Address</label><input type='email' class='form-control' id='email' placeholder='user@example.org'></div><div class='form-group'><label for='company'>Company</label><input type='text' class='form-control' id='company' placeholder='Company Name'></div><div class='form-group'><label for='phone'>Phone</label><input type='tel' class='form-control' id='phone' aria-describedby='emailHelp' placeholder='Phone Number'></div><button type='button' id='submit' class='btn btn-primary' onclick='return submitShort();'>Submit</button></form><br/><div id='ava-form-alert' class='alert alert-warning fade in' style='display: none;'><span id='ava-form-info'></span></div></div>");
+function insertAvaForm() {
+    $('.avaform-wrapper').append('<div style="margin: 20px 20px 20px 20px; padding: 20px 20px 20px 20px;"> <label>Request a Free AvaTax Sandbox Account</label> <form> <div class="form-group"> <label for="name">First Name / Given Name</label> <input type="text" class="form-control" id="firstName" placeholder=""> </div> <div class="form-group"> <label for="name">Last Name / Family Name</label> <input type="text" class="form-control" id="lastName" placeholder=""> </div> <div class="form-group"> <label for="email">Email Address</label> <input type="email" class="form-control" id="email" placeholder="user@example.org"> </div> <div class="form-group"> <label for="company">Company</label> <input type="text" class="form-control" id="company" placeholder="Company Name"> </div> <div class="form-group"> <label for="website">Website</label> <input type="text" class="form-control" id="website" placeholder="Optional"> </div> <div class="form-group"> <label for="phone">Phone</label> <input type="tel" class="form-control" id="phone" aria-describedby="emailHelp" placeholder="Phone Number"> </div> <div class="form-group"> <label for="street-address">Street Address</label> <input type="text" class="form-control" id="street-address" placeholder=""> </div> <div class="form-group" style="float:left;width:32%;margin-right:2%;"> <label for="region">Region</label> <input type="text" class="form-control" id="region" placeholder="Region / State / Province"> </div> <div class="form-group" style="float:left;width:32%;margin-right:2%;"> <label for="country">Country</label> <input type="text" class="form-control" id="country" placeholder="2-Digit Country Code"> </div> <div class="form-group" style="float:left;width:32%;"> <label for="postal-code">Postal Code</label> <input type="text" class="form-control" id="postal-code" placeholder=""> </div> <button type="button" id="submit" class="btn btn-primary" onclick="return submitShort();" style="clear:both;">Submit</button> </form> <br/> <div id="ava-form-alert" class="alert alert-warning fade in" style="display: none;"> <span id="ava-form-info"></span> </div> </div>');
 }
 
-function submitShort()
-{
-    return submitForm($('#firstName').val(), $('#lastName').val(), $('#email').val(), $('#company').val(), $('#phone').val(), $('#ava-form-info'), $('#ava-form-alert'));
+function submitShort() {
+    return submitForm(
+      $('#firstName').val(),
+      $('#lastName').val(),
+      $('#email').val(),
+      $('#company').val(),
+      $('website').val(),
+      $('#phone').val(),
+      $('#street-address').val(),
+      $('#region').val(),
+      $('#country').val(),
+      $('#postal-code').val(),
+      $('#ava-form-info'),
+      $('#ava-form-alert')
+    );
 }
 
-function submitForm(fn, ln, e, c, p, outputElement, alertElement)
-{
+function submitForm(fn, ln, e, c, w, p, l, r, cc, pc, outputElement, alertElement) {
 
     var c_id = 'organic';
-    if(AvaTag.getCombinedData()["CampaignID"]) {
-        c_id = AvaTag.getCombinedData()["CampaignID"];
+
+    if (AvaTag.getCombinedData()['CampaignID']) {
+        c_id = AvaTag.getCombinedData()['CampaignID'];
     }
     console.log("c_id = " + c_id);
     alertElement.hide();
@@ -22,8 +33,15 @@ function submitForm(fn, ln, e, c, p, outputElement, alertElement)
         lastName: ln,
         email: e,
         company: c,
+        website: w,
         phone: p,
-        campaign: c_id        
+        campaign: c_id,
+        companyAddress: {
+            line: l,
+            region: r,
+            country: cc,
+            postalCode: pc
+        }
     };
 
     console.log("payload = ", payload);
@@ -32,23 +50,21 @@ function submitForm(fn, ln, e, c, p, outputElement, alertElement)
         data: JSON.stringify(payload),
         contentType: 'application/json',
         beforeSend: function() {
-            $("#submit").prop('disabled', true); // disable button
+            $('#submit').prop('disabled', true); // disable button
         },
         error: function(err) {
             var avataxerror = $.parseJSON(err.responseText);
             outputElement.html(avataxerror.error.message);
             alertElement.attr('class', 'alert alert-warning fade in');
-            $("#submit").prop('disabled', false);
+            $('#submit').prop('disabled', false);
             alertElement.show();
-            
         },
         dataType: 'json',
         success: function(data) {
             outputElement.html('Success!  Your account credentials have been sent to <strong>' + e + '</strong>.');
             alertElement.attr('class', 'alert alert-success fade in');
-            $("#submit").prop('disabled', false);
+            $('#submit').prop('disabled', false);
             alertElement.show();
-            
         },
         type: 'POST'
     });
@@ -57,8 +73,7 @@ function submitForm(fn, ln, e, c, p, outputElement, alertElement)
 
 // Populate forms on ready
 $(
-    function()
-    {
+    function() {
         insertAvaForm();
     }
 );
