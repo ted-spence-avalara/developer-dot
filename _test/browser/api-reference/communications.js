d8436f974cd27eebe43998f2438c4d2451f287f5
Ted Spence <ted.spence@Avalara.com>
2018-08-06 14:26:13 -0700

    More test tries

--- _test/browser/api-reference/communications.js
+++ _test/browser/api-reference/communications.js
@@ -1,5 +1,5 @@
-//const NUMAPIS = 3;
-//let expectedNumberOfApiEndpoints;
+const NUMAPIS = 2;
+let expectedNumberOfApiEndpoints;
 
 module.exports = {
     'before': function(browser) {
@@ -8,20 +8,20 @@ module.exports = {
 
     'after': function(browser) {
         browser.end();
-    }//,
+    },
 
-//    'API Reference: Communications: REST v1 (verify number of endpoints)': function(browser) {
-//        expectedNumberOfApiEndpoints = 30;
-//
-//        browser
-//            .initialize(browser.globals.baseURL + '/api-reference/communications/afc-swagger-v1/methods/ApiV1ApplicationAFCDatabaseEngineVersionGet/')
-//            .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
-//    },
-//    'API Reference: Communications: REST v2 (verify number of endpoints)': function(browser) {
-//        expectedNumberOfApiEndpoints = 3;
-//
-//        browser
-//            .initialize(browser.globals.baseURL + '/api-reference/communications/afc-swagger-v2/methods/ApiV2AfcCalcTaxesPost/')
-//            .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
-//    }
+    'API Reference: Communications: REST v1 (verify number of endpoints)': function(browser) {
+        expectedNumberOfApiEndpoints = 30;
+
+        browser
+            .initialize(browser.globals.baseURL + '/api-reference/communications/afc-swagger-v1/methods/ApiV1ApplicationAFCDatabaseEngineVersionGet/')
+            .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
+    },
+    'API Reference: Communications: REST v2 (verify number of endpoints)': function(browser) {
+        expectedNumberOfApiEndpoints = 3;
+
+        browser
+            .initialize(browser.globals.baseURL + '/api-reference/communications/afc-swagger-v2/methods/ApiV2AfcCalcTaxesPost/')
+            .apiReference.methods.layout(NUMAPIS, expectedNumberOfApiEndpoints);
+    }
 };
